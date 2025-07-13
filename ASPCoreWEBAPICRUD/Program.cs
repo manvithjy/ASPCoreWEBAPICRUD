2e3c68e52b18285a981fc7bc9496d9bd813c5013
manvithjy <manvithjy99@gmail.com>
2025-07-13 23:50:05 +0530

    created student controller

--- ASPCoreWEBAPICRUD/Program.cs
+++ ASPCoreWEBAPICRUD/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore; // Add this using directive for 'UseSqlServer' extension method
+using ASPCoreWEBAPICRUD.Models;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
